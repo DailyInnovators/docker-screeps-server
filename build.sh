23f3cc32a18165a495b8a641594d3cd350adaa1e
Dome <dome@handy.de>
2019-07-07 15:43:52 +0200

    Auto Update to screeps

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 version=$(curl https://raw.githubusercontent.com/screeps/screeps/master/package.json | jq -r .version)
-git clone git@github.com:ags131/docker-screeps-server
+git clone git@github.com:DailyInnovators/docker-screeps-server.git
 pushd docker-screeps-server
 sed -i 's/^ENV SCREEPS_VERSION.*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
 git commit -a -m "Auto Update to screeps "$version
